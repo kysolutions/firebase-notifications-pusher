4999fb274773b59111cc26558eeb6abaa0688c2a
Serge C. AHOUANSINOU <sahouansinou@gmail.com>
2024-08-22 10:41:21 +0100

    init project

---
+++ requests/NotificationRequest.js
@@ -0,0 +1,29 @@
+const { body, validationResult } = require('express-validator');
+
+class NotificationRequest {
+  static validate() {
+    return [
+      body('token').notEmpty().withMessage('Token is required'),
+      body('title').notEmpty().withMessage('Title is required'),
+      body('body').notEmpty().withMessage('Body is required')
+    ];
+  }
+    
+    static validateMulticast() {
+    return [
+      body('tokens').isArray({ min: 1 }).withMessage('Tokens is required'),
+      body('title').notEmpty().withMessage('Title is required'),
+      body('body').notEmpty().withMessage('Body is required')
+    ];
+  }
+
+  static validateResult(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
+    }
+    next();
+  }
+}
+
+module.exports = NotificationRequest;
